0cbe1e329355e19a221e64f5eef15e874b1eb460
Menelaos Zetas <menelaoszetas@gmail.com>
2017-04-21 20:35:13 +0300

    Changing the UI and adding some Intents at the phone, email and website

--- app/src/main/java/com/example/android/dronecamp_project1/MainActivity.java
+++ app/src/main/java/com/example/android/dronecamp_project1/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.dronecamp_project1;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void callPhone(View view) {
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:" + getString(R.string.phoneText)));
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
+    public void sendMail(View view) {
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_EMAIL, getString(R.string.emailText));
+        intent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.emailSubjectText));
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
+    public void webPage(View view) {
+        Uri webpage = Uri.parse(getString(R.string.websiteHttp));
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
 }
